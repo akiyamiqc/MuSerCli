9ff70f471e2f9c3cf7089f7a7f9a990aaf06f007
qusseei <qusseei@users.noreply.github.com>
2022-01-09 20:20:30 +0800

    Create 3_2_sync_udp_server.cpp

---
+++ src/3_2_sync_udp_server.cpp
@@ -0,0 +1,41 @@
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::udp;
+
+std::string make_daytime_string()
+{
+    using namespace std; // for time_t, time and ctime;
+    time_t now = time(0);
+    char str[126];
+    ctime_s(str, sizeof(str), &now);
+    return str;
+}
+
+int main()
+{
+    try
+    {
+        boost::asio::io_context io_context;
+        udp::socket socket(io_context, udp::endpoint(udp::v4(), 13));
+        for (;;)
+        {
+            boost::array<char, 1> recv_buf;
+            udp::endpoint remote_endpoint;
+            size_t len = socket.receive_from(boost::asio::buffer(recv_buf), remote_endpoint);
+            std::string message = make_daytime_string();
+            boost::system::error_code ignored_error;
+            socket.send_to(boost::asio::buffer(message),
+                           remote_endpoint, 0, ignored_error);
+        }
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
